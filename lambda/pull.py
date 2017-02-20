461a85f87c08ffc695eb190ed740e945f1e4f3cf
lordmuffin <dorkmeisterx69>
2017-02-19 18:04:38 -0600

    Tweaks

--- lambda/pull.py
+++ lambda/pull.py
@@ -4,36 +4,12 @@ import urllib2
 import boto3
 import sys
 import json
-import cfnresponse
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     print "event.dump = " + json.dumps(event)
-    # If nto valid cloudformation custom resource call
+    # If not valid cloudformation custom resource call
 
-
-    print "test"
-    if not event['bucket_name'] or not event['git_url'] or not event['key']:
-        cfnresponse.send(event, context, cfnresponse.FAILED, "Bucket Name, Git URL and Key are all required.", '')
-        return
-
-    response_status = cfnresponse.SUCCESS
-    results = {}
-
-    try:
-
-        for keys,values in event.items():
-            if "bucket_name" in keys:
-                bucket_name = values
-            if "git_url" in keys:
-                git_url = values
-            if "key" in keys:
-                key = values
-        response = urllib2.urlopen (git_url.encode('utf-8'))
-        zipcontent = response.read()
-        s3.Bucket(bucket_name).put_object(Key=key, Body=zipcontent)
-    except Exception as e:
-        print # coding=utf-8
-
-    finally:
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, 'Completed Success', '')
+    response = urllib2.urlopen (event["ResourceProperties"]["git_url"].encode('utf-8'))
+    zipcontent = response.read()
+    s3.Bucket(event["ResourceProperties"]["bucket_name"]).put_object(Key=event["ResourceProperties"]["key"], Body=zipcontent)
