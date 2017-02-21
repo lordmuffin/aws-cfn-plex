14aeb6a05b022aa6ddf672c29d6ede3d424b3adc
lordmuffin <dorkmeisterx69>
2017-02-21 00:10:50 -0600

    V1.0 - WORKS

--- lambda/pull.py
+++ lambda/pull.py
@@ -4,12 +4,18 @@ import urllib2
 import boto3
 import sys
 import json
+import cfnresponse
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     print "event.dump = " + json.dumps(event)
+    responseData = {}
     # If not valid cloudformation custom resource call
-
-    response = urllib2.urlopen (event["ResourceProperties"]["git_url"].encode('utf-8'))
-    zipcontent = response.read()
-    s3.Bucket(event["ResourceProperties"]["bucket_name"]).put_object(Key=event["ResourceProperties"]["key"], Body=zipcontent)
+    try:
+        response = urllib2.urlopen (event["ResourceProperties"]["git_url"].encode('utf-8'))
+        zipcontent = response.read()
+        s3.Bucket(event["ResourceProperties"]["bucket_name"]).put_object(Key=event["ResourceProperties"]["key"], Body=zipcontent)
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, ".zip pulled to S3 Bucket!")
+    except Exception:
+        cfnresponse.send(event, context, cfnresponse.FAILED, responseData, "Bucket Name and Key are all required.")
+        print "ERROR"
