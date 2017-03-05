61b2a0b1695015a7b1edbe9bf2a11197cbad8787
Andrew Jackson <dorkmeisterx69@gmail.com>
2017-03-04 22:35:44 -0600

    updates for travis-ci

--- lambda/git.py
+++ lambda/git.py
@@ -5,8 +5,9 @@ import urllib2
 import boto3
 import sys
 import json
-import cfnresponse
 import git
+import os
+import shutil
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
@@ -16,14 +17,26 @@ def lambda_handler(event, context):
     try:
         git.Git().clone(event["ResourceProperties"]["git_url"])
 
-# Foreach Object in the cloned folder, upload to s3 cloned folder.
+        DIR_NAME = "tmp/git"
+        REMOTE_URL = event["ResourceProperties"]["git_url"]
 
-        s3.Bucket(event["ResourceProperties"]["bucket_name"]).put_object(Key=event["ResourceProperties"]["key"], Body=zipcontent)
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, ".zip pulled to S3 Bucket!")
+        if os.path.isdir(DIR_NAME):
+            shutil.rmtree(DIR_NAME)
+
+        os.mkdir(DIR_NAME)
+
+        repo = git.Repo.init(DIR_NAME)
+        origin = repo.create_remote('origin',REMOTE_URL)
+        origin.fetch()
+        origin.pull(origin.refs[0].remote_head)
+
+        print "---- DONE ----"
+
+# Foreach Object in the cloned folder, upload to s3 cloned folder.
+        for filename in os.listdir('DIR_NAME'):
+            buffer+= open(filename, 'rU').read()
+            s3.Bucket(event["ResourceProperties"]["bucket_name"]).put_object(Key=filename, Body=buffer)
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, responseData, ".zip pulled to S3 Bucket!")
     except Exception:
         cfnresponse.send(event, context, cfnresponse.FAILED, responseData, "Bucket Name and Key are all required.")
         print "ERROR"
-
-
-import git
-git.Git().clone("git://gitorious.org/git-python/mainline.git")
